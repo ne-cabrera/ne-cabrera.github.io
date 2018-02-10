426c350b2396314464a133985a1f835a6f74f539
necabrera <ne.cabrera@uniandes.edu.co>
2018-02-09 22:55:35 -0500

    ya casito

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 var offset = 50;
-
 $('.nav-pills li a').click(function(event) {
     event.preventDefault();
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
+});
+
+$(".descripcion").hide();
+$(".imgProyectos").on("click", function(){
+    $(this).next().slideToggle(400);
 });
